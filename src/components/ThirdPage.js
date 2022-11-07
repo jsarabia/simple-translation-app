37e5c179f9f5b6e8c570334be151d84b059364fd
Joe <jsarabia1247@gmail.com>
2022-11-07 15:36:42 -0500

    download text as usfm

--- src/components/ThirdPage.js
+++ src/components/ThirdPage.js
@@ -3,10 +3,26 @@ import ChunkingView from './ChunkingView';
 import ChunkText from '../domain/ChunkText';
 import MyDraft from '../domain/MyDraft';
 import ChunkButton from './ChunkButton';
+import PrepareDownloadText from '../domain/PrepareDownloadText'
 
 function ThirdPage({isActive}) {
 
-    const [draft, setDraft] = useState("no");
+    const [draft, setDraft] = useState("");
+
+    function saveTextAsFile(textToWrite, fileNameToSaveAs) {
+        function destroyClickedElement(event){
+            document.body.removeChild(event.target);
+        }
+    	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'}); 
+    	var downloadLink = document.createElement("a");
+    	downloadLink.download = fileNameToSaveAs;
+    	downloadLink.innerHTML = "Download File";
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    	downloadLink.click();
+    }
 
     useEffect(() => {
         document.getElementById("final").value = MyDraft.getTranslation();
@@ -15,7 +31,9 @@ function ThirdPage({isActive}) {
     return (
         <div>
             <textarea id="final" rows="40" cols="100">{draft}</textarea>
-            <button onClick={() => alert("what") + isActive}>download</button>
+            <button onClick={() => {
+                saveTextAsFile(PrepareDownloadText.process("jas", 1, document.getElementById("final").value), "doc.usfm");
+            }}>download</button>
       </div>
       )
 }
---
+++ src/domain/PrepareDownloadText.js
@@ -0,0 +1,29 @@
+function stripExcessNewlines(textToProcess) {
+    const regex = /\n+/g;
+    return textToProcess.replace(regex, "\n");
+}
+
+function convertNewlinesToVerseMarkers(textToProcess) {
+    let verses = textToProcess.split("\n");
+    let builder = "";
+    for (let i = 0; i < verses.length; i++) {
+        builder += `\\v ${i + 1} ${verses[i]}\n`;
+    }
+    return builder;
+}
+
+function prependHeader(bookSlug, chapterNumber, draftText) {
+    return `\\toc3 ${bookSlug}
+
+\\c ${chapterNumber}
+${draftText}`;
+}
+
+export default {
+    process(bookSlug, chapterNumber, draftText) {
+        let outputText = stripExcessNewlines(draftText);
+        outputText = convertNewlinesToVerseMarkers(outputText);
+        outputText = prependHeader(bookSlug, chapterNumber, outputText);
+        return outputText;
+    }
+}
