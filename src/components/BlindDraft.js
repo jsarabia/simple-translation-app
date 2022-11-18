6b6e16262cda7118a5e63bc88b06d3bdc8940723
Joe <jsarabia1247@gmail.com>
2022-11-17 21:36:13 -0500

    Update BlindDraft.js

--- src/components/BlindDraft.js
+++ src/components/BlindDraft.js
@@ -2,28 +2,32 @@ import { useEffect, useState } from 'react';
 import myDraft from '../domain/MyDraft';
 
 function BlindDraft({ isActive }) {
-    const [chunk, setChunk] = useState({});
+    const [sourceText, setSourceText] = useState("");
+    const [draftText, setDraftText] = useState("");
 
     const [chunkNumber, setChunkNumber] = useState(0);
 
     useEffect(() => {
-        setChunk(myDraft.getNextChunk(0));
-        setChunkNumber(0)
+        if (isActive) {
+            const chunk = myDraft.getNextChunk(0)
+            setSourceText(chunk.text);
+            setDraftText(chunk.translation);
+        }
     }, [isActive]);
 
     return (
         <div>
-            <div id="chunkText">{chunk.text}</div>
-            <textarea id="draftText" rows="20" cols="100" onInput={() => {
+            <div id="chunkText">{sourceText}</div>
+            <textarea id="draftText" rows="20" cols="100" value={draftText} onInput={() => {
                 document.getElementById("chunkText").style.visibility = "hidden";
             }}></textarea>
             <button onClick={() => {
                 let translationText = document.getElementById('draftText');
                 myDraft.addTranslation(chunkNumber, translationText.value);
                 const newChunk = myDraft.getNextChunk(chunkNumber + 1);
-                setChunk(newChunk);
+                setSourceText(newChunk.text);
+                setDraftText(newChunk.translation);
                 setChunkNumber(chunkNumber + 1)
-                translationText.value = newChunk.translation;
                 document.getElementById("chunkText").style.visibility = "visible";
             }}>Next</button>
         </div>
