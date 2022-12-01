47682d007b404f91c188e37102a7a89efdeb9b83
Joe <jsarabia1247@gmail.com>
2022-12-01 18:05:15 -0500

    Upload button on App Bar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,16 +1,26 @@
-import { IconButton } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import HomeIcon from '@mui/icons-material/Home';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
+import { importUSFM } from "../domain/ImportFile";
+
 
 function AppBar() {
     return (
         <div class="app_bar">
             <IconButton class="nav_btn">
-                <HomeIcon/>
+                <HomeIcon />
             </IconButton>
-            <IconButton class="nav_btn">
-                <FileUploadIcon/>
-            </IconButton>
+        
+            <Button variant="contained" component="label" sx={{margin: 1, minWidth:48, maxWidth: 48, height: 48}}>
+                <input hidden type="file" accept=".usfm, .usfm3, .USFM, .USFM3" onChange={(event) => {
+                    const files = event.target.files;
+                    for (const file of files) {
+                        importUSFM(file);
+                    }
+                }}>
+                </input>
+                <FileUploadIcon />
+            </Button>
         </div>
     );
 }
