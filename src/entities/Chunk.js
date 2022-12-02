1456462f29772ffed1003ed6649de43b22603af7
Joe <jsarabia1247@gmail.com>
2022-12-02 11:16:43 -0500

    Updates

--- src/entities/Chunk.js
+++
@@ -1,9 +0,0 @@
-export class Chunk {
-    constructor(startWord, endWord, text = "", translation = "") {
-        this.start = startWord;
-        this.end = endWord;
-        this.text = text;
-        this.translation = translation
-        this.blindDrafted = translation !== "";
-    }
-}
