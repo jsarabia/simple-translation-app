99454ed3a8ad3ccfb452105ac8a0e270e74690a6
Joe <jsarabia1247@gmail.com>
2022-12-14 14:53:55 -0500

    format content by paragraph
    highlighting off by 1

--- src/components/ChunkingView.jsx
+++ src/components/ChunkingView.jsx
@@ -5,7 +5,7 @@ import { useDraft, DRAFT_ACTIONS } from '../context/upload/DraftContext';
 
 function ChunkingView({ isActive }) {
 
-  const [{draft, status}, reducer] = useDraft();
+  const [{ draft, status }, reducer] = useDraft();
   const [highlightedElement, setHighlightedElement] = useState()
   const [text, setText] = useState([]);
   const [firstRenderDone, setFirstRenderDone] = useState(false);
@@ -29,34 +29,35 @@ function ChunkingView({ isActive }) {
 
   let out = [];
 
-  let count = 0
+  let count = 0;
 
   for (let t of text) {
     var words = t.split(" ");
-    for (let word of words) {
-      count++;
-      // 65 ch 
-      // margin 0 auto
-      out.push((<span class="draft_text" chunked="false" id={count} onClick={(event) => {
-        if (highlightedElement != null) {
-          let elem = document.getElementById(highlightedElement.id);
-          if (elem.getAttribute("chunked") === "false") {
-            elem.style.backgroundColor = highlightedElement.previousColor;
-          }
-        }
+    out.push(
+      <p className="paragraph_text">
+        {words.map((word) => {
+          let spannedText = (<span class="draft_text" chunked="false" id={count} onClick={(event) => {
+            if (highlightedElement != null) {
+              let elem = document.getElementById(highlightedElement.id);
+              if (elem.getAttribute("chunked") === "false") {
+                elem.style.backgroundColor = highlightedElement.previousColor;
+              }
+            }
 
-        let current = event.target.id;
-        let elem = document.getElementById(current);
-        if (elem.getAttribute("chunked") === "false") {
-          let prevColor = elem.style.backgroundColor;
-          elem.style.backgroundColor = "#DDDD0080";
-          setHighlightedElement({ id: current, previousColor: prevColor });
-        }
-      }}>{word} </span>));
-      out.push((<span class="draft_text"> </span>));
-    }
-    count++;
-    out.push((<br chunked="false" id={count}></br>));
+            let current = event.target.id;
+            let elem = document.getElementById(current);
+            if (elem.getAttribute("chunked") === "false") {
+              let prevColor = elem.style.backgroundColor;
+              elem.style.backgroundColor = "#DDDD0080";
+              setHighlightedElement({ id: current, previousColor: prevColor });
+            }
+          }}>{word} </span>);
+          count++;
+          return spannedText;
+        })}
+      </p>
+    );
+    out.push((<span class="draft_text"> </span>));
   }
 
   return (
