6f068d0c0a96af2ac442f2b3eecb5e981e7dfbaf
Joe <jsarabia1247@gmail.com>
2022-12-14 06:55:21 -0500

    Add changes from PR

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'example.react2android',
+  appName: 'react2android',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
